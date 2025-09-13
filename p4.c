ce42a92ded13144be5139414c60b81fa412b4db3
HemanshuDholariya <hemanshu0068@gmail.com>
2025-09-13 21:54:48 +0530

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,19 @@
+/* program : show them the magic of data type and their ranges
+programmer : hemanshu dhola`riya
+date : 21 july 2025
+*/
+#include <stdio.h>
+#incude <conio.h>
+void main()
+{
+	int i;
+	char c;
+	float f;
+	double d;
+	printf("int size is %d",sizeof(i));
+	printf("char is size of %d",sizeof(c));
+	printf("float size is %d",sizeof(f));
+	printf("double size is %d",sizeof(d));
+	getch();
+
+}
