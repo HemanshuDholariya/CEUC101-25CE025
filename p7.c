7b3cf4f6a63e2069d17c20264f9d6cac53cd7ca0
HemanshuDholariya <hemanshu0068@gmail.com>
2025-09-13 22:00:19 +0530

    Create p7.c

---
+++ p7.c
@@ -0,0 +1,17 @@
+/*program : to make an aotomated ticketing system
+programmer : hemanshu dholariya
+date : 25 july 2025
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("enter the your age:");
+  scanf("%d",&a);
+  a<=12?printf("you are eligible"):printf("you are not eligible");
+
+  getch();
+
+  }
