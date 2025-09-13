df0bcea9f6e0ce42b1a418bcbccc0f8b3257690d
HemanshuDholariya <hemanshu0068@gmail.com>
2025-09-13 21:48:43 +0530

    Create P1.c

---
+++ P1.c
@@ -0,0 +1,12 @@
+/* program : print to hello world
+programmer : hemanshu dholariya
+date : 11 september 2025 
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   clrscr();
+   printf("hello world");
+   getch();
+}
